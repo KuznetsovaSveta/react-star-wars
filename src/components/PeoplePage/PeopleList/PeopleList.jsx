49cd8709f7efd70adc11c5171d0c344209f56756
Svetlana Kuznetsova <KuznetsovaSveta0408@gmail.com>
2026-02-02 18:58:24 +0300

    fixed paths for images in PeopleList

--- src/components/PeoplePage/PeopleList/PeopleList.jsx
+++ src/components/PeoplePage/PeopleList/PeopleList.jsx
@@ -8,7 +8,7 @@ const PeopleList = ({people}) => {
     <ul className={styles.list__container}>
         {people.map(({id, name, img}) => 
         <li key={name} className={styles.list__item}>
-            <NavLink to={`/people/${id}`} className={styles.person__link}><img src={`/img/${id}.webp`} alt={name} className={styles.person__phono}/>
+            <NavLink to={`/people/${id}`} className={styles.person__link}><img src={`img/${id}.webp`} alt={name} className={styles.person__phono}/>
             <p className={styles.person__name}>{name}</p></NavLink>
         </li>)}
     </ul>
