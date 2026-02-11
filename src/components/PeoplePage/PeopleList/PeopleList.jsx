8d1af213e484766f9705dec64ad92ec61e804c26
Svetlana Kuznetsova <KuznetsovaSveta0408@gmail.com>
2026-02-11 14:07:29 +0300

    added default image

--- src/components/PeoplePage/PeopleList/PeopleList.jsx
+++ src/components/PeoplePage/PeopleList/PeopleList.jsx
@@ -1,23 +1,33 @@
-import PropTypes from 'prop-types';
-import { NavLink } from 'react-router';
+import PropTypes from "prop-types";
+import { NavLink } from "react-router";
 
-import styles from './PeopleList.module.css';
+import styles from "./PeopleList.module.css";
 import { getBaseUrl } from "@utils/baseUrl.js";
 
-const PeopleList = ({people}) => {
-  return(
+const PeopleList = ({ people }) => {
+  return (
     <ul className={styles.list__container}>
-        {people.map(({id, name, img}) => 
+      {people.map(({ id, name, img }) => (
         <li key={name} className={styles.list__item}>
-            <NavLink to={`/people/${id}`} className={styles.person__link}><img src={`${getBaseUrl()}img/${id}.webp`} alt={name} className={styles.person__phono}/>
-            <p className={styles.person__name}>{name}</p></NavLink>
-        </li>)}
+          <NavLink to={`/people/${id}`} className={styles.person__link}>
+            <img
+              src={`${getBaseUrl()}img/${id}.webp`}
+              alt={name}
+              className={styles.person__photo}
+              onError={(e) => {
+                e.target.src = `${getBaseUrl()}img/default.webp`;
+              }}
+            />
+            <p className={styles.person__name}>{name}</p>
+          </NavLink>
+        </li>
+      ))}
     </ul>
-  )
-}
+  );
+};
 
 PeopleList.propTypes = {
   people: PropTypes.array,
-}
+};
 
 export default PeopleList;
